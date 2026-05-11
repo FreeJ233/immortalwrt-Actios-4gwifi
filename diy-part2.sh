877f90afd4cb1e536d670cde230ca23548bbc7f5
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2026-05-12 01:09:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,3 +20,27 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini package/luci-app-cpu-status-mini
 # git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
 # git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
+
+# luci-app-daed
+# 1. 先删掉可能存在的旧版本（有些源码里可能叫 luci-app-daed 或 daed）
+rm -rf feeds/luci/applications/luci-app-daed
+rm -rf package/feeds/luci/luci-app-daed
+rm -rf package/dae  # 如果你之前克隆过，也先删掉
+
+# 2. 按照官方推荐克隆到 package/dae
+git clone --depth 1 https://github.com/QiuSimons/luci-app-daed package/dae
+
+# 自动注入 daed 所需的内核与插件配置
+echo 'CONFIG_KERNEL_DEBUG_INFO=y' >> .config
+echo 'CONFIG_KERNEL_DEBUG_INFO_BTF=y' >> .config
+echo 'CONFIG_KERNEL_CGROUPS=y' >> .config
+echo 'CONFIG_KERNEL_CGROUP_BPF=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-daed=y' >> .config
+echo 'CONFIG_PACKAGE_daed=y' >> .config
+echo 'CONFIG_PACKAGE_kmod-xdp-sockets-diag=y' >> .config
+
+# 修复依赖问题（强制让编译系统检查依赖）
+make defconfig
+
+./scripts/feeds update -a
+./scripts/feeds install -a
